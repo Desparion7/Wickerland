b81f4fcb7083ce6083d0476aa110016e4a9a33b0
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-04-05 14:28:19 +0200

    add useEffect to sroll up when page is change

--- src/screens/Layout.tsx
+++ src/screens/Layout.tsx
@@ -1,11 +1,16 @@
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import MobileNavigation from '../components/MobileNavigation/MobileNavigation';
 import { useMediaQuery } from 'react-responsive';
 
 const Layout = () => {
 	const isDesktop = useMediaQuery({ minWidth: '1000px' });
+	const location = useLocation();
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [location]);
 	return (
 		<div>
 			<Header />
