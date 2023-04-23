c19417b93603729f90e46e3bf39b5a2a2c713c9f
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-04-23 14:58:34 +0200

    add form validation

--- src/screens/OrderScreen.tsx
+++ src/screens/OrderScreen.tsx
@@ -1,14 +1,40 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useCallback } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import styles from './OrderScreen.module.css';
 import { cartItems } from '../app/slices/cartSlice';
 
+interface FormErrors {
+  name?: string;
+  surname?: string;
+  companyName?: string;
+  street?: string;
+  postcode?: string;
+  city?: string;
+  phone?: string;
+  email?: string;
+  message?: string;
+}
+
 const OrderScreen = () => {
   const cartProducts = useSelector(cartItems);
-  const formRef = useRef(null);
+  const formRef = useRef<HTMLFormElement>(null);
 
+  const [customer, setCustomer] = useState<FormErrors>({
+    name: '',
+    surname: '',
+    companyName: '',
+    street: '',
+    postcode: '',
+    city: '',
+    phone: '',
+    email: '',
+    message: '',
+  });
+  const [errors, setErrors] = useState<FormErrors>({});
+  const [status, setStatus] = useState(false);
   const [statusError, setStatusError] = useState(false);
+
   const [selectedValue, setSelectedValue] = useState(
     'Kurier, Pocztex: 29,00 zł'
   );
@@ -16,69 +42,196 @@ const OrderScreen = () => {
   const handleOptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedValue(e.target.value);
   };
+  // function to count full price
   const totalPrice = cartProducts.reduce((acc, obiekt) => {
     return acc + obiekt.qty * obiekt.price;
   }, 0);
-
   let totalPriceWithDelivery: number;
   if (selectedValue === 'Kurier, Pocztex: 29,00 zł') {
     totalPriceWithDelivery = totalPrice + 29;
   } else {
     totalPriceWithDelivery = totalPrice + 39;
   }
+  // function to set payment method
   const handleChangePayment = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPaymentMethod(e.target.value);
   };
-
-  const handlerFormSumbit = () => {
-    if (formRef.current) {
-      // formRef.current.submit();
+  // function to validate form
+  const validate = useCallback(() => {
+    const validationErrors: FormErrors = {};
+    if (!customer.name) {
+      validationErrors.name = 'Pole wymagane';
     }
+    if (!customer.surname) {
+      validationErrors.surname = 'Pole wymagane';
+    }
+    if (!customer.email) {
+      validationErrors.email = 'Pole wymagane';
+    } else if (!/\S+@\S+\.\S+/.test(customer.email)) {
+      validationErrors.email = 'Nieprawidłowy adres e-mail';
+    }
+    if (!customer.street) {
+      validationErrors.street = 'Pole wymagane';
+    }
+    if (!customer.postcode) {
+      validationErrors.postcode = 'Pole wymagane';
+    }
+    if (!customer.city) {
+      validationErrors.city = 'Pole wymagane';
+    }
+    if (!customer.phone) {
+      validationErrors.phone = 'Pole wymagane';
+    } else if (!/^\d{9}$/.test(customer.phone)) {
+      validationErrors.phone = 'Nieprawidłowy numer telefonu';
+    }
+    return validationErrors;
+  }, [customer]);
+
+  // submit form
+  const handlerSendForm = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      const validationErrors = validate();
+
+      if (Object.keys(validationErrors).length === 0) {
+        if (!status) {
+          setStatusError(true);
+          return;
+        }
+        // send to Api
+        //  console.log({});
+        // clean inputs
+        setCustomer({
+          name: '',
+          surname: '',
+          companyName: '',
+          street: '',
+          postcode: '',
+          city: '',
+          phone: '',
+          email: '',
+          message: '',
+        });
+        // clean errors
+        setErrors({});
+      } else {
+        if (!status) {
+          setStatusError(true);
+        }
+        setErrors(validationErrors);
+      }
+    },
+    [validate, status]
+  );
+
+  // function to set change inputs value
+  const handleCustomerChange = (key: string, value: string) => {
+    setCustomer({ ...customer, [key]: value });
+    setErrors({ ...errors, [key]: undefined });
   };
 
   return (
     <section className={styles.orderScreen}>
       <div className={styles.orderScreen_from}>
         <h2>Dane Płatności</h2>
-        <form action="" ref={formRef}>
+        <form action="" ref={formRef} id="myForm" onSubmit={handlerSendForm}>
           <label htmlFor="name">
             Imię <span>*</span>
           </label>
-          <input type="text" id="name" />
+          <input
+            type="text"
+            id="name"
+            value={customer.name}
+            onChange={(e) => handleCustomerChange('name', e.target.value)}
+            className={errors.name ? styles.error_input : ''}
+            placeholder={errors.name ? errors.name : ''}
+          />
           <label htmlFor="surname">
             Nazwisko <span>*</span>
           </label>
-          <input type="text" id="surname" />
+          <input
+            type="text"
+            id="surname"
+            value={customer.surname}
+            onChange={(e) => handleCustomerChange('surname', e.target.value)}
+            className={errors.surname ? styles.error_input : ''}
+            placeholder={errors.surname ? errors.surname : ''}
+          />
           <label htmlFor="companyname">Nazwa firmy(opcjonalnie)</label>
-          <input type="text" id="companyname" />
+          <input
+            type="text"
+            id="companyname"
+            value={customer.companyName}
+            onChange={(e) =>
+              handleCustomerChange('companyName', e.target.value)
+            }
+          />
           <label htmlFor="street">
             Ulica <span>*</span>
           </label>
           <input
             type="text"
             id="street"
-            placeholder="nazwa ulicy,numer budynku/numer lokalu"
+            value={customer.street}
+            onChange={(e) => handleCustomerChange('street', e.target.value)}
+            className={errors.street ? styles.error_input : ''}
+            placeholder={
+              errors.street
+                ? errors.street
+                : 'nazwa ulicy,numer budynku/numer lokalu'
+            }
           />
           <label htmlFor="postcode">
             Kod pocztowy <span>*</span>
           </label>
-          <input type="number" id="postcode" />
+          <input
+            type="text"
+            pattern="[0-9\-]*"
+            id="postcode"
+            value={customer.postcode}
+            onChange={(e) => handleCustomerChange('postcode', e.target.value)}
+            className={errors.postcode ? styles.error_input : ''}
+            placeholder={errors.postcode ? errors.postcode : ''}
+          />
           <label htmlFor="city">
             Miasto <span>*</span>
           </label>
-          <input type="text" id="city" />
+          <input
+            type="text"
+            id="city"
+            value={customer.city}
+            onChange={(e) => handleCustomerChange('city', e.target.value)}
+            className={errors.city ? styles.error_input : ''}
+            placeholder={errors.city ? errors.city : ''}
+          />
           <label htmlFor="phone">
             Numer telefonu <span>*</span>
           </label>
-          <input type="number" id="phone" />
+          <input
+            type="number"
+            id="phone"
+            value={customer.phone}
+            onChange={(e) => handleCustomerChange('phone', e.target.value)}
+            className={errors.phone ? styles.error_input : ''}
+            placeholder={errors.phone ? errors.phone : ''}
+          />
           <label htmlFor="email">
             Adres e-mail <span>*</span>
           </label>
-          <input type="email" id="email" />
+          <input
+            type="email"
+            id="email"
+            value={customer.email}
+            onChange={(e) => handleCustomerChange('email', e.target.value)}
+            className={errors.email ? styles.error_input : ''}
+            placeholder={errors.email ? errors.email : ''}
+          />
           <label htmlFor="comments">Uwagi do zamówienia(opcjonalnie)</label>
           <textarea
             name="comments"
             id="comments"
+            value={customer.message}
+            onChange={(e) => handleCustomerChange('message', e.target.value)}
             placeholder="Uwagi do zamówienia, np. informacje o dostarczeniu przesyłki"
           />
         </form>
@@ -131,7 +284,7 @@ const OrderScreen = () => {
         <div className={styles.orderScreen_order_summary}>
           <h3>Łącznie</h3>
           <div className={styles.orderScreen_order_summary_price}>
-            {totalPriceWithDelivery} zł
+            {totalPriceWithDelivery.toFixed(2)} zł
           </div>
         </div>
         <div className={styles.orderScreen_order_payment}>
@@ -168,19 +321,25 @@ const OrderScreen = () => {
           </p>
         </div>
         <div className={styles.orderScreen_order_statute}>
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            checked={status}
+            onChange={() => {
+              setStatus(!status);
+              setStatusError(false);
+            }}
+          />
           <label>
             Przeczytałem/am i akceptuję <Link to="/regulamin">regulamin</Link>
             <span>*</span>
           </label>
         </div>
         {statusError && (
-          <p className={styles.errorMessage}>
-            Zakup wymaga akceptacji regulaminu
-          </p>
+          <div className={styles.statusError}>
+            Do zakupu niezbędna jest akceptacja regulaminu
+          </div>
         )}
-
-        <button onClick={handlerFormSumbit} type="button">
+        <button type="submit" form="myForm">
           Kupuję i płacę
         </button>
       </div>
